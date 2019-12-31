5b944f9d31e7f17f1dea6415927b974c2367bae0
Ilya Bernshteyn <ilyabe@gmail.com>
2019-12-31 15:04:45 -0600

    Fixed up Karatsuba
    It still wasn't working quite right for very large numbers, so I
    resorted to padding to make sure the number of digits is always a
    power of 2 and that n straight forward to calculate for Karatsuba.

--- src/algorithms_1/week_1.clj
+++ src/algorithms_1/week_1.clj
@@ -24,30 +24,48 @@
 
 ;; Karatsuba Multiplication
 
-(defn halves [x y n]
-  (let [n2 (/ n 2)]
-    (->> [(quot x (Math/pow 10 n2))
-          (rem x (Math/pow 10 n2))
-          (quot y (Math/pow 10 n2))
-          (rem y (Math/pow 10 n2))]
-         (map bigint))))
+(defn halves [x y n2]
+  (let [->num (fn [xs] (bigint (apply str xs)))
+        [a b] (map ->num (split-at n2 (str x)))
+        [c d] (map ->num (split-at n2 (str y)))]
+    [a b c d]))
 
-(defn compute-n [x y]
-  (let [cx (count (str x))
-        cy (count (str y))]
-    (if (and (even? cx) (even? cy))
-      cx
-      (inc (max cx cy)))))
+(defn pad [x y]
+  "Pad x and y with 0s so that n is always a power of 2."
+  (let [xs (str x)
+        ys (str y)
+        cxs (count xs)
+        cys (count ys)
+        mc (max cxs cys)
+        mcev (if (even? mc) mc (inc mc))
+        fmt (str "%0" mcev "d")
+        zs (fn [num] (format fmt (BigInteger. (str num))))]
+    (cond
+      (= 1 cxs cys) [xs ys]
+      (= 2 cxs cys) [xs ys]
+      (and (= cxs cys) (even? cxs)) [xs ys]
+      ;; which is bigger?
+      ;; round to nearest power of 2
+      ;; format with that many zeroes
+      :else [(zs x) (zs y)])))
 
 (comment
-  (let [x 1234 y 5678 n (compute-n x y)] (halves x y n))
-  (let [x 46 y 134 n (compute-n x y)] (halves x y n)))
+  [(pad 1 2)
+   (pad 12 34)
+   (pad 1 23)
+   (pad 12 3)
+   (pad 123 45)
+   (pad 123 456)
+   (pad 123 4567)
+   (pad 1234 567)])
 
 (defn karatsuba [x y]
   (if (or (< x 10) (< y 10))
     (* x y)
-    (let [n (compute-n x y)
-          [a b c d] (halves x y n)
+    (let [[px py] (pad x y)
+          n (count px)
+          n2 (/ n 2)
+          [a b c d] (halves px py n2)
           p (+ a b)
           q (+ c d)
           ac (karatsuba a c)
@@ -55,17 +73,26 @@
           pq (karatsuba p q)
           adbc (- pq ac bd)]
       (+ (* ac (bigint (Math/pow 10 n)))
-         (* adbc (bigint (Math/pow 10 (/ n 2))))
+         (* adbc (bigint (Math/pow 10 n2)))
          bd))))
 
 (comment
-  (karatsuba 46 134) ;; 6164
-  (karatsuba 10 50)
+  (karatsuba 46 134)
+  (karatsuba 51146 51134) ;; 6164
+  (* 51146 51134)
+  (karatsuba 10 50) *e
+  (karatsuba 34 78) ;; => 2652
+  (karatsuba 56 78) ;; => 4368
   (karatsuba 5 6)
   (karatsuba 1112 2224) ;; 2473088
-  (karatsuba 1234N 5678N) ;; 7006652
-  (karatsuba 8385623558792674733786478294439724659735456427989434445674878447N
-             2826463673794433976332997844395283945995575924857989533744856647N)
+  (karatsuba 1234 5678) ;; 7006652
+  (karatsuba 2500 4500)
+  (= 8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184
+     (karatsuba 3141592653589793238462643383279502884197169399375105820974944592
+                2718281828459045235360287471352662497757247093699959574966967627))
+  (= 8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184
+     (* 3141592653589793238462643383279502884197169399375105820974944592N
+        2718281828459045235360287471352662497757247093699959574966967627N))
   (karatsuba 1234 5678)
   (take 2 [1 2 3 4 5])
   (drop 2 [1 2 3 4 5])
@@ -97,7 +124,7 @@
 ;;;; 9 ≤ 8 + 3 - 2
 ;;;; 9 ≤ 9
 
-(defn max 
+(defn lmax 
   "A linear max without using Clojure's built-in max"
   [coll]
   (loop [x 0
@@ -124,7 +151,7 @@
               (knockout c d)))))
 
 (defn second-place [coll]
-  (max (second (winner coll))))
+  (lmax (second (winner coll))))
 
 (comment
   (winner [12 24 6 4 56 32 98 100])
