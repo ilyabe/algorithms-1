9313316b937fe0949cad0689c83c7a7314132297
Ilya Bernshteyn <ilyabe@gmail.com>
2020-01-06 09:31:45 -0600

    Fixed up inversion count to increment properly
    Now it increments the count by the number of elements remaining in
    the left list and the counts return correctly.

--- src/algorithms_1/week_2.clj
+++ src/algorithms_1/week_2.clj
@@ -4,9 +4,6 @@
 ;; Count inversions
 ;;
 
-(defn merge-and-count-split-inv [c d]
-  [[] 0])
-
 ;; Bring in MergeSort and adapt to track counts, not just sort
 (defn merge-and-count-split-inv [a b]
   ;(prn "a" a "b" b)
@@ -21,8 +18,7 @@
                                (update 0 conj x)))
       (< y x) (recur as ys (-> sorted
                                (update 0 conj y)
-                               ;; inc isn't right, should be "inc by count of left"
-                               (update 1 inc))))))
+                               (update 1 #(+ % (count as))))))))
 
 (defn sort-and-count-inv [coll]
   (let [cnt (count coll)
@@ -33,9 +29,11 @@
       :else (let [[c left-inv] (sort-and-count-inv (take mid coll))
                   [d right-inv] (sort-and-count-inv (drop mid coll))
                   [b split-inv] (merge-and-count-split-inv c d)]
-              (prn "left-inv" left-inv "right-inv" right-inv "split-inv" split-inv)
+              ;(prn "left-inv" left-inv "right-inv" right-inv "split-inv" split-inv)
               [b (+ left-inv right-inv split-inv)]))))
 
 (comment
   (sort-and-count-inv [8 7 6 5])
-  (sort-and-count-inv [8 7 6 5 4 3 2 1]))
+  (sort-and-count-inv [8 7 6 5 4 3 2 1])
+  (sort-and-count-inv (read-string (slurp "test/resources/inversions_data_set.clj")))
+  (last (sort-and-count-inv (read-string (slurp "test/resources/hw2.clj")))))
--- test/algorithms_1/week_2_test.clj
+++ test/algorithms_1/week_2_test.clj
@@ -2,8 +2,15 @@
   (:require [clojure.test :refer :all]
             [algorithms-1.week-2 :as w2]))
 
+(def bigger [54044 14108 79294 29649 25260 60660 2995 53777 49689 9083])
+(def hw2 (read-string (slurp "test/resources/hw2.clj")))
+
 (deftest sort-and-count-inv
   (testing "Sanity check: 0 inversions for a sorted array"
     (is (= 0 (second (w2/sort-and-count-inv [0 1 2 3 4 5])))))
   (testing "Sanity check: n(n-1)/2 inversions for a reverse sorted array"
-    (is (= 28 (second (w2/sort-and-count-inv [8 7 6 5 4 3 2 1]))))))
+    (is (= 28 (second (w2/sort-and-count-inv [8 7 6 5 4 3 2 1])))))
+  (testing "Bigger numbers work too"
+    (is (= 28 (second (w2/sort-and-count-inv bigger)))))
+  (testing "Challenge set with 10k numbers"
+    (is (= 2407905288 (second (w2/sort-and-count-inv hw2))))))
